2cdc2f5a898cbe54946d69b30474130f8736577c
derin <derinceyhan25@gmail.com>
2026-05-14 17:20:31 +0200

    Updated Home page

--- frontend/frontend-app/src/pages/Home.jsx
+++ frontend/frontend-app/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 
+
 export default function Home({ setPage, setPlaces }) {
   const [budget, setBudget] = useState("");
   const [duration, setDuration] = useState("");
